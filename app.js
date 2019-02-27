3be1352293e735ebb06a99e373c6166975d091cd
kishan <kishan.ks@hotmail.com>
2019-02-27 16:41:36 -0600

    final commit

--- app.js
+++ app.js
@@ -1,25 +1,58 @@
-console.log('Starting app.js');
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
 
 const notes = require('./notes');
 
-var argv = yargs.argv;
-var command = process.argv[2];
+var tiitleOption = {
+    describe: 'title of note',
+    demand: true,
+    alias: 't'
+}
+var argv = yargs
+        .command('add', 'Add a new note', {
+            title: tiitleOption,
+            body: {
+                describe: 'body of note',
+                demand: true,
+                alias: 'b'
+            }
+        })
+        .command('list', 'List all notes')
+        .command('read', 'Read a note', {
+            title :tiitleOption
+        })
+        .command('remove', 'Delete Above message', {
+            title: tiitleOption
+        })
+        .help()
+        .argv;
+var command = argv._[0];
 
-console.log(`Command: ${command}`);
-console.log('Yargs: ', argv);
-
-if(command === 'add') {
-    notes.addNote(argv.title, argv.body);
-} else if(command === 'list') {
-    notes.getAll();
-} else if(command === 'read') {
-    notes.getNote(argv.title);
-} else if(command === 'remove') {
-    notes.removeNote(argv.title);
+if (command === 'add') {
+    var note = notes.addNote(argv.title, argv.body);
+    if (note) {
+        console.log('Note Created!');
+        notes.logNote(note);
+    } else {
+        console.log('Title is taken');
+    }
+} else if (command === 'list') {
+    var allNotes = notes.getAll();
+    console.log(`Printing ${allNotes.length} note(s).`);
+    allNotes.forEach((note) => notes.logNote(note));
+} else if (command === 'read') {
+    var noteRead = notes.getNote(argv.title);
+    if (noteRead) {
+        console.log('Note Read!');
+        notes.logNote(noteRead);
+    } else {
+        console.log('Note not found');
+    }
+} else if (command === 'remove') {
+    var noteRemoved = notes.removeNote(argv.title);
+    var message = noteRemoved ? 'Note was removed' : 'Note not found';
+    console.log(message);
 } else {
     console.log('Command not not found');
 }
---
+++ playground/json.js
@@ -0,0 +1,16 @@
+ const fs = require('fs');
+
+ var originalNote = {
+     title: 'some title',
+     body: 'some body'
+ };
+
+//  originalNoteString
+var originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('note.json', originalNoteString)
+
+var noteString = fs.readFileSync('note.json');
+var note = JSON.parse(noteString);
+
+console.log(typeof note);
+console.log(note.title);
